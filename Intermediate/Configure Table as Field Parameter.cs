ffc0cfd14ea1b6783935c4cb01556332cff29db5
Bernat Agulló Roselló <40455069+bernatagulloesbrina@users.noreply.github.com>
2025-03-01 17:18:26 +0100

    configure table as field parameter
    make an existing table behave as a field parameter -- it requires a display text column, a field column and an order column

---
+++ Intermediate/Configure Table as Field Parameter.cs
@@ -0,0 +1,26 @@
+//'2024-08-09 / B.Agullo /
+// Configure a field parameter table
+
+if(Selected.Tables.Count() != 1)
+{
+    Error("Select a single table and try again");
+    return;
+}
+Table fieldParameterTable = Selected.Table;
+if (fieldParameterTable.Columns.Count() < 3)
+{
+    Error("This script expects at least 3 columns in the table");
+    return;
+}
+Column displayNameColumn = SelectColumn(fieldParameterTable, fieldParameterTable.Columns[0], "Select display name column");           
+if(displayNameColumn == null) { Error("You cancelled");return; }; 
+Column fieldColumn = SelectColumn(fieldParameterTable, fieldParameterTable.Columns[1], "Select field table");
+if(fieldColumn == null) { Error("You cancelled"); return; };
+Column orderColumn = SelectColumn(fieldParameterTable, fieldParameterTable.Columns[2], "Select order column");
+if(orderColumn == null) { Error("You cancelled"); return; };
+fieldColumn.SetExtendedProperty(name: "ParameterMetadata", value: @"{""version"":3,""kind"":2}", type: ExtendedPropertyType.Json);
+displayNameColumn.GroupByColumns.Add(fieldColumn);
+displayNameColumn.SortByColumn = orderColumn;
+fieldColumn.SortByColumn = orderColumn;
+fieldColumn.IsHidden = true;
+orderColumn.IsHidden = true; 
